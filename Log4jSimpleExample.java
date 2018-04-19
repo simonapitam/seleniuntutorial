7d59064db5fe1a420efa579ce49fbd9cdea07189
simonapitam <simonapitam@gmail.com>
2018-04-19 17:45:36 +0300

    Log4j Example Updated
    With the correct code

--- Log4jSimpleExample.java
+++ Log4jSimpleExample.java
@@ -2,8 +2,35 @@ package Log4j;
 
 import org.testng.annotations.Test;
 
-public class Log4jSimpleExample {
-  @Test
-  public void f() {
-  }
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+ 
+public class Log4jSimpleExample{
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+     BasicConfigurator.configure();
+     Logger Mylogger = LogManager.getLogger("DebugLogger");
+ 
+     //Setting up the log level of both loggers
+      Mylogger.setLevel(Level.DEBUG);
+ 
+      Mylogger.trace("This is the trace log - DEBUG");
+      Mylogger.debug("This is debug log - DEBUG");
+      Mylogger.info("This is info log - DEBUG");
+      Mylogger.warn("This is Warn log - DEBUG");
+      Mylogger.error("This is error log - DEBUG");
+      Mylogger.fatal("This is Fatal log - DEBUG");
+ 
+      Mylogger.setLevel(Level.WARN);
+      Mylogger.trace("This is the trace log - WARN");
+      Mylogger.debug("This is debug log - WARN");
+      Mylogger.info("This is info log - WARN");
+      Mylogger.warn("This is Warn log - WARN");
+      Mylogger.error("This is error log - WARN");
+      Mylogger.fatal("This is Fatal log - WARN");      
+	}
 }
